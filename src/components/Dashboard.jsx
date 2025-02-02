55aba5a125dfc0c75a8f8888386a86b894e6003e
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-02 21:51:34 +0545

    make a simple Dashboard component with logout functionality

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import { useNavigate } from "react-router-dom";
+import { account } from "../lib/appwrite";
 const Dashboard = () => {
+  const navigate = useNavigate();
+  async function logout() {
+    try {
+      let response = await account.deleteSession("current");
+      console.log("Logged out!!",response)
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+      
+    }
+  }
+  // return !isUserLoggedIn ? navigate("/") : <div>hello</div>;
   return (
     <div>
-      
+      <h1>Dashboard</h1>
+      <button
+        type="submit"
+        className=" p-2 bg-purple-600 rounded hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 cursor-pointer"
+        onClick={(e) => {
+          e.preventDefault();
+          logout();
+        }}
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
