8f399d50c9225bc6b4df4e7625e7551aea2781db
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-03 15:27:25 +0545

    update About component for improved text alignment and readability

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,17 +8,17 @@ const Home = () => {
     <div className="min-h-screen flex flex-col   bg-gray-900 text-white ">
       <main className="flex-grow flex flex-col items-center  p-14">
         <h2 className="text-4xl mb-4">About Me</h2>
-        <p className="text-lg mb-8 text-slate-400">
+        <p className="text-lg mb-8 text-justify md:text-left text-slate-400">
           Hello! I'm Narendra Mahara, a passionate software developer with
           experience in building web applications. I love to learn new
           technologies and improve my skills.
         </p>
-        <p className="text-lg mb-8 text-slate-400">
+        <p className="text-lg mb-8  text-justify md:text-left text-slate-400">
           I have worked on various projects involving different technologies and
           frameworks. My goal is to create efficient and scalable applications
           that provide a great user experience.
         </p>
-        <p className="text-lg mb-8 text-slate-400">
+        <p className="text-lg mb-8  text-justify md:text-left text-slate-400">
           In my free time, exploring new tools and libraries.
         </p>
 
